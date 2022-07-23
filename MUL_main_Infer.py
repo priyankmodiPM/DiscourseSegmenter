137c905bcc7528a5abaaf79a7bf895c0912ac48f
Priyank Modi <priyank.modi@research.iiit.ac.in>
2022-07-23 10:42:44 +0530

    loop over all files

--- MUL_main_Infer.py
+++ MUL_main_Infer.py
@@ -6,6 +6,8 @@ import os
 import config
 from transformers import AutoTokenizer, AutoModel
 from model_depth import ParsingNet
+import json
+import codecs
 
 os.environ["CUDA_VISIBLE_DEVICES"] = str(config.global_gpu_id)
 
@@ -34,7 +36,7 @@ def inference(model, tokenizer, input_sentences, batch_size):
             EndPosition = (loop + 1) * batch_size
             if EndPosition > len(input_sentences):
                 EndPosition = len(input_sentences)
-
+           # print(StartPosition, EndPosition)
             input_sen_batch = input_sentences[StartPosition:EndPosition]
             _, _, SPAN_batch, _, predict_EDU_breaks = model.TestingLoss(input_sen_batch, input_EDU_breaks=None, LabelIndex=None,
                                                                         ParsingIndex=None, GenerateTree=True, use_pred_segmentation=True)
@@ -64,10 +66,24 @@ if __name__ == '__main__':
     model = model.cuda()
     model.load_state_dict(torch.load(model_path))
     model = model.eval()
+    
+    directory = "./matres_data/"
+    for filename in os.listdir(directory):
+        f = os.path.join(directory, filename)
+        data = {}
+        if os.path.isfile(f):
+            InputSentences = open(f).readlines()
+            input_sentences, all_segmentation_pred, all_tree_parsing_pred = inference(model, bert_tokenizer, InputSentences, batch_size)
+            f_out = "./output/"+filename.split('.txt')[0]
+            data['tokens'] = input_sentences
+            data['edus'] = all_segmentation_pred
+            json_string = json.dumps(data)
+            with codecs.open(f_out+'.json', 'w', encoding='utf-8') as outfile:
+                json.dump(json_string, outfile, ensure_ascii=False)
+            #f.close()
+    #Test_InputSentences = open("./data/text_for_inference.txt").readlines()
 
-    Test_InputSentences = open("./data/text_for_inference.txt").readlines()
-
-    input_sentences, all_segmentation_pred, all_tree_parsing_pred = inference(model, bert_tokenizer, Test_InputSentences, batch_size)
-    print(input_sentences[0])
-    print(all_segmentation_pred[0])
-    print(all_tree_parsing_pred[0])
+    #input_sentences, all_segmentation_pred, all_tree_parsing_pred = inference(model, bert_tokenizer, Test_InputSentences, batch_size)
+    #print(input_sentences)
+    #print(all_segmentation_pred)
+    #print(all_tree_parsing_pred)
